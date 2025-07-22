b5a553ffd3ba5741ef0fecc9797089cc8128ef17
TuRan <TuRanPROC@outlook.com>
2025-07-22 19:42:29 +0800

    update

---
+++ LeetCode/30.串联所有单词的子串.cpp
@@ -0,0 +1,16 @@
+// 参考资料: https://leetcode.cn/problems/substring-with-concatenation-of-all-words/solutions/3691292/30-ci-ding-chang-hua-dong-chuang-kou-pyt-5vgx/
+
+/*
+ * @lc app=leetcode.cn id=30 lang=cpp
+ *
+ * [30] 串联所有单词的子串
+ */
+
+// @lc code=start
+class Solution {
+public:
+	vector<int> findSubstring(string s, vector<string> &words)
+	{
+	}
+};
+// @lc code=end
