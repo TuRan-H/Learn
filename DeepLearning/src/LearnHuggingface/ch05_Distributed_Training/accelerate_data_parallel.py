17247f70647339966afd14e3f2cd13b6316a8866
TuRan <TuRanPROC@outlook.com>
2025-11-07 15:49:10 +0800

    更新 .gitignore 文件以包含 wandb；更新 requirements.txt 添加 wandb 和 deepspeed 依赖；重构 arguments.py 以支持新的训练参数；新增 accelerate_config.yaml 和 deepspeed_configs.json 配置文件以支持分布式训练。

--- DeepLearning/src/LearnHuggingface/ch05_Distributed_Training/accelerate_data_parallel.py
+++ DeepLearning/src/LearnHuggingface/ch05_Distributed_Training/accelerate_data_parallel.py
@@ -2,7 +2,7 @@
 使用accelerate框架进行并行训练
 """
 
-import os
+import math
 import torch as tc
 import numpy as np
 import pandas as pd
@@ -12,9 +12,6 @@ from transformers import BertTokenizer, BertForSequenceClassification
 from torch.utils.data import Dataset, DataLoader, random_split
 from torch.optim import Adam
 from src.LearnHuggingface.ch05_Distributed_Training.arguments import Arguments
-
-
-LOCAL_RANK = os.environ["LOCAL_RANK"]
 
 
 class MyDataset(Dataset):
@@ -46,11 +43,10 @@ def prepare_dataloader(args: Arguments):
         inputs["labels"] = tc.tensor(labels)
         return inputs
 
-    trainloader = DataLoader(trainset, batch_size=32, collate_fn=collate_func, shuffle=True)
-    validloader = DataLoader(validset, batch_size=64, collate_fn=collate_func, shuffle=False)
+    trainloader = DataLoader(trainset, batch_size=args.batch_size, collate_fn=collate_func, shuffle=True)
+    validloader = DataLoader(validset, batch_size=args.batch_size, collate_fn=collate_func, shuffle=False)
 
     return trainloader, validloader
-
 
 
 def prepare_model_and_optimizer(args: Arguments):
@@ -65,7 +61,7 @@ def prepare_model_and_optimizer(args: Arguments):
 def evaluate(model, validloader, accelerator: Accelerator):
     model.eval()
     acc_num = 0
-    with tc.inference_mode():
+    with tc.no_grad():
         for batch in validloader:
             output = model(**batch)
             pred = tc.argmax(output.logits, dim=-1)
@@ -76,35 +72,92 @@ def evaluate(model, validloader, accelerator: Accelerator):
 
 def train(model, optimizer, train_loader, valid_loader, accelerator: Accelerator, args: Arguments):
     global_step = 0
-    logs_loss = np.array(0)
+    log_loss = np.array(0)
     bar = None
 
-    if LOCAL_RANK == 0:
-        bar = tqdm(total=args.num_epoch * len(train_loader))
+    if accelerator.is_main_process:
+        bar = tqdm(total=math.ceil(args.num_epoch * len(train_loader) / args.gradient_accumulation_steps))
 
-    for ep in range(args.num_epoch):
+    for _ in range(args.num_epoch):
         model.train()
         for batch in train_loader:
-            output = model(**batch)
-            loss = output.loss
-            accelerator.backward(loss)
-            optimizer.step()
-            if global_step % args.log_step == 0:
-                logs_loss = loss.detach().cpu().numpy()
+            with accelerator.accumulate(model):
+                output = model(**batch)
+                loss = output.loss
+                # ATTN: 使用accelerator包装模型进行分布式训练后, 不要再使用loss.backward()了, 因为这样无法做到梯度的all-reduce. 使用accelerator.backward()代替
+                accelerator.backward(loss)
+                optimizer.step()
+                optimizer.zero_grad()
 
-            if LOCAL_RANK == 0:
-                assert isinstance(bar, tqdm)
-                bar.update(1)
-                bar.set_postfix({"loss": logs_loss})
+                if accelerator.sync_gradients:
+                    global_step += 1
+                    # ATTN: 分布式条件下, 使用reduce来获取所有进程的 mean loss
+                    log_loss = accelerator.reduce(loss, reduction="mean")
+                    log_loss = log_loss.item()  # type: ignore
+
+                    if global_step % args.save_steps == 0:
+                        accelerator.save_state(f"{args.output_dir}/step_{global_step}")
+                        accelerator.unwrap_model(model).save_pretrained(
+                            save_directory=f"{args.output_dir}/step_{global_step}",
+                            is_main_process=accelerator.is_main_process,
+                            state_dict=accelerator.get_state_dict(model),
+                            save_func=accelerator.save,
+                        )  # type: ignore
+
+                    if accelerator.is_main_process:
+                        accelerator.log(
+                            {"train/loss": log_loss, "global_step": global_step},
+                            step=global_step,
+                        )
+
+                        assert isinstance(bar, tqdm)
+                        bar.update(1)
+                        bar.set_postfix({"loss": log_loss})
+
+        acc = evaluate(model, valid_loader, accelerator)
+        accelerator.log(
+            {"eval/accuracy": acc, "global_step": global_step},
+            step=global_step,
+        )
+
+    if accelerator.is_main_process:
+        assert isinstance(bar, tqdm)
+        bar.close()
+
 
 def main():
     args = Arguments()
-    accelerator = Accelerator()
+
+    accelerator = Accelerator(
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        mixed_precision="bf16",
+        log_with="wandb",
+    )
+
+    accelerator.init_trackers(
+        project_name=args.project_name,
+        init_kwargs={
+            "wandb": {
+                "name": args.run_name,
+                "dir": args.log_dir,
+            }
+        },
+    )
+
     train_dataloader, valid_dataloader = prepare_dataloader(args)
     model, optimizer = prepare_model_and_optimizer(args)
-    model, optimizer, train_dataloader, valid_dataloader = accelerator.prepare(model, optimizer, train_dataloader, valid_dataloader)
+    # ATTN: 使用accelerator.prepare包装 model, optimizer, dataloader, scheduler (可选)
+    model, optimizer, train_dataloader, valid_dataloader = accelerator.prepare(
+        model,
+        optimizer,
+        train_dataloader,
+        valid_dataloader,
+    )
 
     train(model, optimizer, train_dataloader, valid_dataloader, accelerator, args)
+
+    # ATTN: 结束分布式训练, 这行代码很关键, 防止显存泄漏
+    accelerator.end_training()
 
 
 if __name__ == "__main__":
--- DeepLearning/src/LearnHuggingface/ch05_Distributed_Training/arguments.py
+++ DeepLearning/src/LearnHuggingface/ch05_Distributed_Training/arguments.py
@@ -1,11 +1,27 @@
+import os
 from dataclasses import dataclass, field
+from datetime import datetime
 
 
 @dataclass
 class Arguments:
+    project_name: str = field(default="text_classification_v3")
     dataset_path: str = field(default="./datasets/ChnSentiCorp_htl_all.csv")
     model_name_or_path: str = field(default="./models/rbt3")
     output_dir: str = field(default="./results/text_classification_v3")
+    log_dir: str = field(default="./logs/text_classification_v3")
+    run_name: str = field(default="")
     num_epoch: int = field(default=3)
     log_step: int = field(default=10)
     lr: float = field(default=2e-5)
+    batch_size: int = field(default=32)
+    gradient_accumulation_steps: int = field(default=2)
+    save_steps: int = field(default=20)
+
+    def __post_init__(self):
+        current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+        self.output_dir = os.path.join(self.output_dir, current_time)
+        self.run_name = current_time
+
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir, exist_ok=True)
